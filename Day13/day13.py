ed9cdcc4b9a678f80a189fb51aeb24c6b12ed5e1
Ruben Negrin <rnegrin1990@gmail.com>
2017-12-13 18:15:23 +0000

    day 13, all done but writeup missing, also the code is pretty awful since I did it against the clock, so needs refactoring too

--- Day13/day13.py
+++ Day13/day13.py
@@ -32,25 +32,60 @@ def move_security(firewall):
                 firewall[key][level-1] = True, 1
 
 
-def break_firewall(start, firewall):
+def break_firewall(start, delay, firewall, stop_when_caught=False):
     level = 0
     pos = start - 1
     picoseconds = 0
     end = max(firewall.keys())
     caught_layers = set()
-    while pos <= end:
-        pos += 1
-        if pos in firewall and firewall[pos][level] != False:
-            caught_layers.add(pos)
+    for i in range(0, delay):
         move_security(firewall)
         picoseconds += 1
-    return firewall, level, pos, caught_layers
+    while pos <= end:
+        pos += 1
+        if pos in firewall and firewall[pos][level] is not False:
+            caught_layers.add(pos)
+            if stop_when_caught:
+                print(f"caught at {delay}")
+                break
+        move_security(firewall)
+        picoseconds += 1
+    return caught_layers
 
 
 def process(input):
     firewall = parse_input(input)
-    _, level, pos, detections = break_firewall(0, firewall)
+    detections = break_firewall(0, 0, firewall)
     return sum(list(map(lambda p: p*len(firewall[p]), detections)))
+
+
+def reset_firewall(firewall):
+    for key in firewall.keys():
+        firewall[key] = [False] * len(firewall[key])
+        firewall[key][0] = True, 0
+
+
+def get_guard_position(start, turns, length):
+    freq = (length-1)*2
+    return (turns % freq) + start
+
+
+def detected(firewall, delay):
+    for key in firewall:
+        if get_guard_position(0, delay+key, len(firewall[key])) == 0:
+            return True
+    return False
+
+
+def process_two(input):
+    firewall = parse_input(input)
+    delay = 0
+    while True:
+        if detected(firewall, delay):
+            delay += 1
+        else:
+            break
+    return delay
 
 
 test_input = """0: 3
@@ -60,3 +95,7 @@ test_input = """0: 3
 assert process(test_input.split('\n')) == 24
 
 print(process(get_input()))
+
+assert process_two(test_input.split('\n')) == 10
+
+print(process_two(get_input()))
